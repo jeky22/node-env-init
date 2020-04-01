012978685c1b973028ddc4c60502cfa8de7b7516
jeky22 <qhzh33@gmail.com>
2020-04-01 15:56:33 +0900

    webpack setting

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+import express from 'express'
+
+const port = process.env.PORT || 3030
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
